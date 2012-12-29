3cee5c8c2a735c8cafebea6aa7a88777058e278f
Kenneth Reitz <me@kennethreitz.com>
2012-12-29 03:46:20 -0500

    simple

--- blackbox.py
+++ blackbox.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 
+
 import os
+import json
+import time
+from datetime import datetime
+from uuid import uuid4
 
 from pyelasticsearch import ElasticSearch
 from flask import Flask
@@ -8,6 +13,34 @@ from flask import Flask
 app = Flask(__name__)
 
 es = ElasticSearch(os.environ['ELASTICSEARCH'])
+
+
+class Record(object):
+    def __init__(self):
+        self.uuid = str(uuid4())
+        self.content_type = 'application/octet-stream'
+        self.epoch = epoch()
+        self.file_name = None
+        self.ref = None
+        self.links = {}
+        self.metadata = {}
+
+    @property
+    def content(self):
+        pass
+
+    def save(self):
+        pass
+
+    def __repr__(self):
+        return '<Record {0}>'.format(self.uuid)
+
+
+def epoch(dt=None):
+    if not dt:
+        dt = datetime.utcnow()
+
+    return time.mktime(dt.timetuple()) * 1000 + dt.microsecond / 1000
 
 
 @app.route('/')
@@ -18,8 +51,18 @@ def hello():
 def hello2():
     return 'Hello World!'
 
-@app.route('/search/')
-def hello3():
+@app.route('/collections/', methods=['GET'])
+def get_collection():
+    return 'Hello World!'
+
+
+@app.route('/collections/', methods=['POST'])
+def post_collection():
+    return 'Hello World!'
+
+
+@app.route('/collections/', methods=['POST'])
+def put_collection():
     return 'Hello World!'
 
 if __name__ == '__main__':
