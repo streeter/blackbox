f265351e142194298818643dbbdb03a50929a82d
Kenneth Reitz <me@kennethreitz.com>
2012-12-29 03:00:06 -0500

    simple

---
+++ manage.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from flask.ext.script import Manager
+
+from myapp import app
+
+manager = Manager(app)
+
+@manager.command
+def hello():
+    print "hello"
+
+if __name__ == "__main__":
+    manager.run()
