1eb0243de2c9b932e658de0340e37e6c74007b75
Kenneth Reitz <me@kennethreitz.com>
2012-12-29 06:54:21 -0500

    purge_index, seed_index

--- manage.py
+++ manage.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python
 
 from flask.ext.script import Manager
+from clint.textui import progress
 
-from myapp import app
+from blackbox import *
+
+def iter_metadata():
+    for key in bucket.list():
+        if key.name.endswith('.json'):
+            yield key
+
 
 manager = Manager(app)
 
@@ -12,6 +19,19 @@ manager = Manager(app)
 @manager.command
 def hello():
     print "hello"
+
+
+@manager.command
+def purge_index():
+    es.delete_index('archives')
+
+@manager.command
+def seed_index():
+    print 'Indexing:'
+    for key in progress.bar([i for i in iter_metadata()]):
+        r = Record.from_uuid(key.name[:-5])
+        r.index()
+
 
 if __name__ == "__main__":
     manager.run()
