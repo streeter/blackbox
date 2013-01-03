49b94f16873b0a09261b4cdf293bedb43fe8f7e9
Kenneth Reitz <me@kennethreitz.com>
2013-01-03 17:17:17 -0500

    flickr.

--- importers/flickr.py
+++ importers/flickr.py
@@ -17,11 +17,65 @@ Options:
 from _util import *
 from docopt import docopt
 
-def main(update=False, dry=False):
-    URL = 'http://api.flickr.com/services/rest/?method=flickr.people.getPhotos&user_id=me&per_page=500&format=json'
+URL = 'https://secure.flickr.com/services/rest/?method=flickr.people.getPhotos&user_id=me&per_page=100&format=json&nojsoncallback=1&extras=description,date_upload,date_taken,original_format,geo,path_alias,url_o'
+
+def lookup_record(photo):
+
+    try:
+        return blackbox.iter_search('metadata.service:flickr AND metadata.id:{}'.format(photo['id'])).next()
+    except Exception:
+        return None
+
+
+def iter_photos():
 
     r = foauth.get(URL)
-    print r.content
+    j = r.json()
+
+    for page in range(j['photos']['pages']):
+        r = foauth.get(URL, params={'page': page})
+        j = r.json()
+        for photo in j['photos']['photo']:
+            yield photo
+
+
+
+def main(update=False, dry=False):
+
+    for photo in iter_photos():
+
+        existing = lookup_record(photo)
+        if existing:
+            print 'Existing:',
+
+            if not update:
+                print '{0}. \nExiting.'.format(existing)
+                return
+
+
+        r = existing or blackbox.Record()
+
+        r.content_type = 'image/jpeg'
+        r.ref = 'http://www.flickr.com/photos/{}/{}'.format(photo['pathalias'], photo['id'])
+        r.description = u'Flickr photo. {}'.format(photo['title'])
+        r.author = 'Kenneth Reitz'
+        r.links['src'] = photo['url_o']
+        r.epoch = int(photo[u'dateupload'])*1000
+
+        r.metadata['id'] = photo['id']
+        r.metadata['service'] = 'flickr'
+        r.metadata['title'] = photo['title']
+        r.metadata['taken'] = epoch(parse(photo[u'datetaken']))
+        r.metadata['longitude'] = photo['longitude']
+        r.metadata['latitude'] = photo['latitude']
+
+        print r
+
+        if not dry:
+            r.save()
+
+            r.upload_task.delay(r, url=photo['url_o'])
+
 
 
 if __name__ == '__main__':
