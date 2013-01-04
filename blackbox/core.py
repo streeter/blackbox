c4c3bc6be87b68bf5f3158b9590de1596a2d5ad2
Kenneth Reitz <me@kennethreitz.com>
2013-01-04 01:16:45 -0500

    typo

--- blackbox/core.py
+++ blackbox/core.py
@@ -274,10 +274,10 @@ def get_records():
     def gen():
         for result in results:
             d = result.dict
-            d.paths['path:meta'] = url_for('get_record', uuid=result.uuid)
-            d.paths['path:data'] = url_for('download_record', uuid=result.uuid)
-            d.paths['archive:meta'] = result.meta_archive
-            d.paths['archive:data'] = result.content_archive
+            d['links']['path:meta'] = url_for('get_record', uuid=result.uuid)
+            d['links']['path:data'] = url_for('download_record', uuid=result.uuid)
+            d['links']['archive:meta'] = result.meta_archive
+            d['links']['archive:data'] = result.content_archive
 
             yield d
 
